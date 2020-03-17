6f62cfdede745e2ae40b46989b36ded2722135a4
Akos Somodi <akossomodi@yahoo.com>
2020-03-17 17:20:59 +0100

    create event listeners

--- script.js
+++ script.js
@@ -9,4 +9,10 @@ const swap = document.getElementById('swap');
 // Fetch exchange rates and update the DOM
 function calculate() {}
 
+// Event listeners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+
 calculate();
